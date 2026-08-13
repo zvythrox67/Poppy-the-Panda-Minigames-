ff8e98c30e421dbc6fe56ca42c6687d714f2e9a7
Judith Mohan <147111213+zvythrox67@users.noreply.github.com>
2026-08-12 21:21:51 -0400

    Title Screen and Level Screen

---
+++ Global.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var minigames_done = 0
+var lives = 5
+var completed_games = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
